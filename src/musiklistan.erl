e93b227965a08e85c4fcee1068bc7347e730b2b4
kristian@purestyle.se <kristian@purestyle.se>
2015-05-16 18:21:01 +0200

    moved DB communication to separate module

--- src/musiklistan.erl
+++ src/musiklistan.erl
@@ -1,4 +1,20 @@
 -module(musiklistan).
--export([init/0]).
+-export([put_kv/2, get_v/1]).
 
-init() -> "hej".
+-define(b2l, binary_to_list).
+-define(BUCKET, <<"my bucket">>).
+-define(localhost, "127.0.0.1").
+-define(RIAK_PORT, 8087).
+
+put_kv(Key, Value) ->
+    {ok, Pid} = riakc_pb_socket:start_link(?localhost, ?RIAK_PORT),
+    Object    = riakc_obj:new(?BUCKET,
+                              Key,
+                              Value),
+    riakc_pb_socket:put(Pid, Object, [{w, 2}, {dw, 1}, return_body]).
+
+get_v(Key) ->
+    {ok, Pid} = riakc_pb_socket:start_link(?localhost, ?RIAK_PORT),
+    {ok, {riakc_obj, _Bucket, _Key, _Bin, [{_Riak, Value}], _, _}}
+        = riakc_pb_socket:get(Pid, ?BUCKET, Key),
+    Value.
