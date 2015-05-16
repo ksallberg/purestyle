4f4d437a3c8701d0bae01addcd07167f28c42923
kristian@purestyle.se <kristian@purestyle.se>
2015-05-16 18:59:24 +0200

    100 riak requests, now also closing the socket :)

--- src/musiklistan.erl
+++ src/musiklistan.erl
@@ -6,15 +6,24 @@
 -define(localhost, "127.0.0.1").
 -define(RIAK_PORT, 8087).
 
-put_kv(Key, Value) ->
+riak_connect() ->
     {ok, Pid} = riakc_pb_socket:start_link(?localhost, ?RIAK_PORT),
+    Pid.
+
+riak_disconnect(Pid) ->
+    riakc_pb_socket:stop(Pid).
+
+put_kv(Key, Value) ->
+    Pid       = riak_connect(),
     Object    = riakc_obj:new(?BUCKET,
                               Key,
                               Value),
-    riakc_pb_socket:put(Pid, Object, [{w, 2}, {dw, 1}, return_body]).
+    riakc_pb_socket:put(Pid, Object, [{w, 2}, {dw, 1}, return_body]),
+    riak_disconnect(Pid).
 
 get_v(Key) ->
-    {ok, Pid} = riakc_pb_socket:start_link(?localhost, ?RIAK_PORT),
+    Pid = riak_connect(),
     {ok, {riakc_obj, _Bucket, _Key, _Bin, [{_Riak, Value}], _, _}}
         = riakc_pb_socket:get(Pid, ?BUCKET, Key),
+    riak_disconnect(Pid),
     Value.
