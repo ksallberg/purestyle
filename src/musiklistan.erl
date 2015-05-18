e821a1da3624c77c1769fc552729e892da926f6d
kristian@purestyle.se <kristian@purestyle.se>
2015-05-18 23:12:52 +0200

    added DB schema sketch

--- src/musiklistan.erl
+++ src/musiklistan.erl
@@ -6,7 +6,32 @@
 -define(l2b, list_to_binary).
 -define(b2l, binary_to_list).
 
--record(usercookie, {username, times=0}).
+-record(usercookie, {username, times = 0}).
+
+%% Database schema used for this app:
+%%
+%% 1) All users bucket
+%% -----------------------
+%% | username | password |
+%% -----------------------
+%% | someuser | ******   |
+%% | user2    | ***      |
+%% -----------------------
+%%
+%% 2) All lists of a user: (example bucket name: user2_lists)
+%% -----------------
+%% | id | listname |
+%% -----------------
+%% | 1  | rock     |
+%% | 2  | techno   |
+%% -----------------
+%%
+%% 3) Actual list (associated with a user) bucket: user2_list_rock
+%% ---------------------------------------
+%% | id | URL                            |
+%% ---------------------------------------
+%% | 1  | http://www.youtube.com/jwdaijd |
+%% ---------------------------------------
 
 reg_user(Username, Password) ->
     Bucket = <<"users">>,
@@ -26,7 +51,7 @@ check_login(Username, Password) ->
             case ?b2l(DBPassword) == Password of
                 false -> login_fail;
                 true  ->
-                    CookieRecord = #usercookie{username=Username},
+                    CookieRecord = #usercookie{username = Username},
                     Cookie       = yaws_api:new_cookie_session(CookieRecord),
                     C = yaws_api:set_cookie("usersession",
                                             Cookie,
