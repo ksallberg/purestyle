aa6e8774514f5eb51ab1e0d5de6506077ed9f464
kristian@purestyle.se <kristian@purestyle.se>
2015-06-06 10:17:19 +0200

    moved js to own file, and now have a holder for the container (hmm strange naming)

--- pages/playlist.js
+++ pages/playlist.js
@@ -1,5 +1,92 @@
-// https://developers.google.com/youtube/iframe_api_reference
+var currentTrack = -1;
+var youtubePlayer;
 
-function next_track() {
-    //find next track and start it
+function nextTrack() {
+    currentTrack ++;
+    changeTrack();
+}
+
+function prevTrack() {
+    currentTrack --;
+    changeTrack();
+}
+
+function goto(trackid) {
+    currentTrack = trackid;
+    changeTrack();
+}
+
+function changeTrack() {
+    for(var i = 0; i < tracks.length; i ++) {
+        document.getElementById(tracks[i].id)
+                .style.backgroundColor = 'transparent';
+    }
+
+    document.getElementById(tracks[currentTrack].id)
+            .style.backgroundColor = 'blue';
+
+    emit_player(tracks[currentTrack].id, tracks[currentTrack].source);
+}
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+function onPlayerStateChange(event) {
+    console.log(event);
+    if(event.data == 0)
+        nextTrack();
+}
+
+function getYoutube(id) {
+    clearContainer();
+
+    //Finally, add the youtube player
+    player = new YT.Player('container', {
+        height: '390',
+        width: '640',
+        videoId: id,
+        events: {
+            'onReady':       onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+function getSoundcloud(id) {
+    clearContainer();
+
+    var t = "<iframe id=\"sound\" width=\"50% \" height=\"300\" " +
+            "scrolling=\"no\"" +
+            "frameborder=\"no\" src=\"https://w.soundcloud.com/player/"
+            + "?url=https%3A//api.soundcloud.com/tracks/" + id +
+            "&amp;auto_play=true&amp;hide_related=false&amp;" +
+            "show_comments=true&amp;show_user=true&amp;show_reposts="+
+            "false&amp;visual=true\"></iframe>";
+
+    document.getElementById("container").innerHTML = t;
+
+    //Apply for song finished event
+    var widget = SC.Widget("sound");
+    widget.bind(SC.Widget.Events.FINISH, function(eventData) {
+        nextTrack();
+    });
+}
+
+function clearContainer() {
+    // Remove whatever is in the container
+    var frame = document.getElementById("container");
+    frame.parentNode.removeChild(frame);
+
+    // And create a new container
+    var newDiv = document.createElement('div');
+    newDiv.id = 'container';
+    document.getElementById("holder").appendChild(newDiv);
+}
+
+function emit_player(text, source) {
+    if(source == "youtube")
+        getYoutube(text);
+    if(source == "soundcloud")
+        getSoundcloud(text);
 }
