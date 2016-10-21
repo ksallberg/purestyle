c3529a02b7b8dd1d8e400c2f9acecf3603491568
kristian@purestyle.se <kristian@purestyle.se>
2016-10-22 00:27:12 +0200

    redirect http to https

---
+++ src/redir.erl
@@ -0,0 +1,43 @@
+%% Copyright (c) 2014-2016, Kristian Sällberg
+%% All rights reserved.
+%%
+%% Redistribution and use in source and binary forms, with or without
+%% modification, are permitted provided that the following conditions are met:
+%%
+%% * Redistributions of source code must retain the above copyright notice, this
+%%   list of conditions and the following disclaimer.
+%%
+%% * Redistributions in binary form must reproduce the above copyright notice,
+%%   this list of conditions and the following disclaimer in the documentation
+%%   and/or other materials provided with the distribution.
+%%
+%% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+%% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+%% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+%% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+%% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+%% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+%% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+%% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+%% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+%% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+%% OF THE POSSIBILITY OF SUCH DAMAGE.
+
+-module(redir).
+
+-behaviour(http_handler).
+
+-export([ init/0
+        , routes/0
+        ]).
+
+init() ->
+    ok.
+
+routes() ->
+    [ {html, get,  "/", fun handle_index/3} ].
+
+handle_index(_Data, _Parameters, _Headers) ->
+    #{response      => <<"">>,
+      extra_headers => "Location: https://www.purestyle.se\r\n",
+      return_code   => "301 Moved Permanently"}.
