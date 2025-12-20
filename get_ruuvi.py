5ed7fc9782579efbae107dfee2f098b22566f7f3
Kristian Sallberg <krisallb@cisco.com>
2025-12-20 20:37:38 +0100

    bump sensor timeout

--- get_ruuvi.py
+++ get_ruuvi.py
@@ -6,7 +6,7 @@ def timeout_handler(signum, frame):
     raise TimeoutError("Sensor read timed out")
 
 signal.signal(signal.SIGALRM, timeout_handler)
-signal.alarm(10)  # 10 seconds timeout
+signal.alarm(20)  # 20 seconds timeout
 
 try:
     sensor = RuuviTag("F9:09:24:57:1E:87")
